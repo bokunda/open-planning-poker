c02b960a474c4b271bfb7f6c112b2a5eacaf9f7d
Bojan Piskulić <bpiskulic@qubitsoft.rs>
2025-03-10 23:48:19 +0100

    Refactor user service

--- backend/OpenPlanningPoker/src/Shared/OpenPlanningPoker.Shared/Models/BaseUserProfile.cs
+++ backend/OpenPlanningPoker/src/Shared/OpenPlanningPoker.Shared/Models/BaseUserProfile.cs
@@ -4,4 +4,7 @@ public class BaseUserProfile(Guid id, string userName)
 {
     public Guid Id { get; set; } = id;
     public string UserName { get; set; } = userName;
+
+    public string GetCacheKey() => $"User:{Id}";
+    public static string GetCacheKey(Guid id) => $"User:{id}";
 }
--- backend/OpenPlanningPoker/src/Shared/OpenPlanningPoker.Shared/Services/CacheUserService.cs
+++ backend/OpenPlanningPoker/src/Shared/OpenPlanningPoker.Shared/Services/CacheUserService.cs
@@ -2,9 +2,31 @@
 
 public class CacheUserService(HybridCache cache) : IUserService
 {
-    public async Task<BaseUserProfile?> GetUserProfileAsync(string userId, CancellationToken cancellationToken = default) =>
+    public async Task<BaseUserProfile?> GetAsync(string id, CancellationToken cancellationToken = default) =>
         await cache.GetOrCreateAsync(
-            $"User:{userId}",
+            $"User:{id}",
             async cancellationToken => await Task.FromResult<BaseUserProfile?>(null),
             cancellationToken: cancellationToken);
+
+    public async Task<BaseUserProfile> AddAsync(string username, CancellationToken cancellationToken = default)
+    {
+        var user = new BaseUserProfile(Guid.NewGuid(), username);
+        await cache.SetAsync(user.GetCacheKey(), user, cancellationToken: cancellationToken);
+        return user;
+    }
+
+    public async Task<BaseUserProfile?> UpdateAsync(string id, string newUserName, CancellationToken cancellationToken = default)
+    {
+        var user = await GetAsync(id, cancellationToken);
+
+        if (user is null) 
+        { 
+            return null; 
+        }
+
+        user.UserName = newUserName;
+        await cache.SetAsync(user.GetCacheKey(), user, cancellationToken: cancellationToken);
+
+        return user;
+    }
 }
