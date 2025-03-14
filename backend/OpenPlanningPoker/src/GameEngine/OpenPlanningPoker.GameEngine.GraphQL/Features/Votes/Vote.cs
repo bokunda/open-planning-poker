46fcfc81cb595968456cb7f493d546db039a8577
Bojan Piskulić <bpiskulic@qubitsoft.rs>
2025-03-14 03:28:15 +0100

    Bugfix

--- backend/OpenPlanningPoker/src/GameEngine/OpenPlanningPoker.GameEngine.GraphQL/Features/Votes/Vote.cs
+++ backend/OpenPlanningPoker/src/GameEngine/OpenPlanningPoker.GameEngine.GraphQL/Features/Votes/Vote.cs
@@ -4,5 +4,5 @@ public class Vote
 {
     public Guid Id { get; set; }
     public Guid PlayerId { get; set; }
-    public int Value { get; set; }
+    public string Value { get; set; } = string.Empty;
 }
