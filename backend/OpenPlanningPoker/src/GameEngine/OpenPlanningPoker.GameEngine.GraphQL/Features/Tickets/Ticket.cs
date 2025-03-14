99af23fbb4f2b79aab2369e9f16990ccea17f56e
Bojan Piskulic <bpiskulic1996@gmail.com>
2025-03-15 00:45:58 +0100

    Display voting history (#72)
    Co-authored-by: Bojan Piskulić <bpiskulic@qubitsoft.rs>

--- backend/OpenPlanningPoker/src/GameEngine/OpenPlanningPoker.GameEngine.GraphQL/Features/Tickets/Ticket.cs
+++ backend/OpenPlanningPoker/src/GameEngine/OpenPlanningPoker.GameEngine.GraphQL/Features/Tickets/Ticket.cs
@@ -6,4 +6,39 @@ public class Ticket
     public Guid GameId { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public ApiCollection<Vote> Votes { get; set; } = new([], 0);
+    
+    public async Task<ApiCollection<Vote>> GetVotes(
+        [Service] ISender sender,
+        [Service] IMapper mapper,
+        CancellationToken cancellationToken)
+    {
+        var result = await sender.Send(new GetVotesQuery(Id), cancellationToken);
+        Votes = mapper.Map<ApiCollection<Vote>>(result.Value);
+        return Votes;
+    }
+
+    public async Task<decimal> GetAverageVotingValue(
+        [Service] ISender sender,
+        [Service] IMapper mapper,
+        CancellationToken cancellationToken)
+    {
+        Votes ??= await GetVotes(sender, mapper, cancellationToken);
+
+        var rawVotes = Votes.Items.Select(v => v.Value);
+        var total = 0M;
+        var count = 0;
+
+        foreach (var vote in rawVotes)
+        {
+            if (!decimal.TryParse(vote, out var parsedVote)) 
+            { 
+                continue;
+            }
+
+            total += parsedVote;
+            count++;
+        }
+        return count > 0 ? total / count : 0;
+    }
 }
--- backend/OpenPlanningPoker/src/GameEngine/OpenPlanningPoker.GameEngine.GraphQL/Features/Votes/Vote.cs
+++ backend/OpenPlanningPoker/src/GameEngine/OpenPlanningPoker.GameEngine.GraphQL/Features/Votes/Vote.cs
@@ -5,4 +5,10 @@ public class Vote
     public Guid Id { get; set; }
     public Guid PlayerId { get; set; }
     public string Value { get; set; } = string.Empty;
+
+    public async Task<string> GetPlayerName(
+        [Service] IUserService userService,
+        CancellationToken cancellationToken = default) =>
+        (await userService.GetAsync(PlayerId.ToString(), cancellationToken))
+            ?.UserName ?? string.Empty;
 }
