0ea707086c66b939f652e75e70891353a308804f
Bojan Piskulić <bpiskulic@qubitsoft.rs>
2025-03-22 02:01:21 +0100

    Migrate frontend project to separate repository

--- frontend/open-planning-poker-ui/src/app/shared/constants.ts
+++
@@ -1,5 +0,0 @@
-export const OPP_GITHUB_REPO_URL = 'https://github.com/bokunda/open-planning-poker';
-export const OPP_WEBSITE_URL = 'https://openplanningpoker.com';
-
-export const OPP_SNACKBAR_DURATION_DEFAULT = 2000;
-export const OPP_SNACKBAR_MODAL_LABEL_CLOSE = 'Close';
